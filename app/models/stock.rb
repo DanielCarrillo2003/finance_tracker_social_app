6f82a75d51ef901e492f4646037c5ecea8063929
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-11 13:45:34 -0600

    Show stock table

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 require 'httparty'
 
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
 
     def self.company_lookup(ticker_symbol)
         response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_client[:api_key]}")
