af4dab1e1d0fd57e7ebd5878817fefa069c069c7
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-11 12:55:36 -0600

    AJAX responses handled

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,26 @@
 class StocksController < ApplicationController 
+    protect_from_forgery except: :search
+
     def search 
         if params[:stock].present? 
             if params[:stock].length <= 5 && params[:stock].length >= 2
-                @stock = Stock.new_lookup(params[:stock])
-                render 'users/my_portfolio'
+                @stock = Stock.new_lookup(params[:stock]) 
+                if @stock 
+                    respond_to do |format|
+                        format.js {render partial: 'users/result'}
+                    end
+                end
             else 
-                flash[:alert] = "It seems that the symbol you have entered does not exist, check if you spelled it correctly"
-                redirect_to my_portfolio_path
+                respond_to do |format|
+                flash.now[:alert] = "It seems that the symbol you have entered does not exist, check if you spelled it correctly"
+                format.js {render partial: 'users/result'}
+                end
             end
         else 
-            flash[:alert] = "Please enter a symbol to search"
-            redirect_to my_portfolio_path
+            respond_to do |format|
+                flash.now[:alert] = "Please enter a symbol to search"
+                format.js {render partial: 'users/result'}
+                end
         end
     end
     
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,8 @@ const webpack = require("webpack");
 environment.plugins.append(
   "Provide",
   new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
     Popper: ["popper.js", "default"],
   })
 );
