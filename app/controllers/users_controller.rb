efeb3b7b22ad9556eee519aa570fc822d5eb5988
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-12 00:22:20 -0600

    Search working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  protect_from_forgery except: :search
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
@@ -7,8 +9,25 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search
-    render json: params[:friend]    
+  def search 
+    if params[:friend].present? 
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends 
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else 
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else 
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
-  
 end
